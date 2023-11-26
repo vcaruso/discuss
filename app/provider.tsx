6c2424a213e083cd6755e1195667a0b47e4c058d
vincenzocaruso <enzo@carusovincenzo.com>
2023-11-26 15:23:46 +0100

    setting oauth2

---
+++ app/provider.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+import { NextUIProvider } from '@nextui-org/react'
+import { SessionProvider } from 'next-auth/react'
+
+type ProvidersProps = {
+    children: React.ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <SessionProvider>
+      <NextUIProvider>
+          { children }
+      </NextUIProvider>
+    </SessionProvider>
+  )
+}
---
+++ components/profile.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react'
+import { useSession } from 'next-auth/react';
+
+export default function Profile() {
+    const session = useSession();
+    if(session.data?.user){
+        return <div>From Client: {JSON.stringify(session.data)}</div>
+    }
+  return (
+    <div>From client: user is NOT signed in</div>
+  )
+}
