09152377cc464b15656dfc7e805d17aac6db2443
vcaruso <vcaruso@xeniaprogetti.it>
2023-11-27 10:13:32 +0100

    Complete Create Topic Forms

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -1,5 +1,74 @@
 'use server';
-export async function createTopic(){
-    // TODO: revalidate the home after creating a topic
-    console.log("Create Topic has been called")
+
+import type { Topic } from '@prisma/client';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { db } from '@/db';
+import paths from '@/paths';
+import { revalidatePath } from 'next/cache';
+import { useForceUpdate } from 'framer-motion';
+
+const createTopicSchema = z.object({
+    name: z.string().min(3).regex(/^[a-z+$]/, {message: 'Must be lowercase or dashes without spaces' }),
+    description: z.string().min(10)
+
+});
+
+type CreateTopicFormState = {
+    errors: {
+        name?: string[],
+        description?: string[],
+        _form?: string[]
+    }
+}
+
+export async function createTopic(formState: CreateTopicFormState, formData: FormData): Promise<CreateTopicFormState>{
+    const result = createTopicSchema.safeParse({
+        name: formData.get('name'),
+        description: formData.get('description')
+    });
+    if(!result.success){
+        return {errors: result.error.flatten().fieldErrors};
+    }
+
+    const session = await auth();
+    if(!session || !session.user){
+        return {
+            errors:{
+                _form: ['You must be signed in to do this.']
+            }
+        }
+    }
+    let topic: Topic;
+    try {
+      
+        topic = await db.topic.create({
+            data: {
+                slug: result.data.name,
+                description: result.data.description
+            }
+        });
+
+    } catch(err: unknown) {
+        if(err instanceof Error){
+            return {
+                errors: {
+                    _form: [err.message]
+                }
+            }
+        } else {
+            return {
+                errors: {
+                    _form: ['Something went wrong.']
+                }
+            }
+        }
+        
+    }
+
+    revalidatePath(paths.home());    
+    redirect(paths.topicShow(topic.slug));
+
+
 }
--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useFormState } from 'react-dom';
 import React from 'react';
 import {
   Input,
@@ -11,17 +13,36 @@ import {
 import * as actions from '@/actions';
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {errors:{}});
   return (
     <Popover placement='left'>
       <PopoverTrigger>
           <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-          <form action={actions.createTopic}>
+          <form action={action}>
             <div className='flex flex-col gap-4 p-4 w-80'>
               <h3 className='text-lg'>Create a Topic</h3>
-              <Input label="Name" labelPlacement='outside' placeholder='Name'/>
-              <Textarea label="Description" labelPlacement='outside' placeholder='Describe your topic'/>
+              <Input 
+                  name="name" 
+                  label="Name" 
+                  labelPlacement='outside' 
+                  placeholder='Name' 
+                  errorMessage={formState.errors.name?.join(', ')} 
+                  isInvalid={!!formState.errors.name} 
+              />
+              <Textarea name="description" 
+                  label="Description" 
+                  labelPlacement='outside' 
+                  placeholder='Describe your topic' 
+                  isInvalid={!!formState.errors.description} 
+                  errorMessage={formState.errors.description?.join(', ')}
+              />
+              {
+                formState.errors._form && (
+                  <div className="rounded p-2 bg-red-200 border border-red-400">{formState.errors._form.join(', ')}</div>
+                )
+              }
               <Button type="submit">Submit</Button>
             </div>
             
